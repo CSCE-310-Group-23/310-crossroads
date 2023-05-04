441017b09dfda2c44006911f735e073f68b8466c
Catherine <catherineduong22@gmail.com>
2023-05-04 12:22:39 -0500

    commit home page for customers

---
+++ customer_acc/cust_home_page/cust_home_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>inserting PHP code into html</title>
+</head>
+<body>
+<h2>Hello, this is your customers' page for items</h2>
+<h2> Select Action:
+<form action = "http://localhost/CS_310_DEMO/customer_acc/item_view_desc/item_view_desc.html">
+    <button>
+        View item's description
+    </button>
+</form>
+<form action = "http://localhost/CS_310_DEMO/customer_acc/item_list_search/item_search.html">
+    <button>
+        Look up item's information
+    </button>
+</form>
+</h2>
+</body>
+</html>
