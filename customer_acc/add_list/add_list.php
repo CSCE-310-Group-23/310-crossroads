dfc029fecda510bb085cdf33ad0961b12480d0d0
Catherine <catherineduong22@gmail.com>
2023-05-05 17:43:08 -0500

    update add_list functionality

---
+++ customer_acc/add_list/add_list.php
@@ -0,0 +1,55 @@
+
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$con = mysqli_connect('localhost', 'root', '','crossroads');
+
+// get the post records
+
+$txtItem = $_POST['txtItem'];
+$txtOrder = $_POST['txtOrder'];
+$txtItemPrice = $_POST['txtItemPrice'];
+$txtItemQuantity = $_POST['txtItemQuantity'];
+$txtlistprice = (int)$txtItemPrice * (int)$txtItemQuantity;
+//$txtItemPrice= $_POST['txtItemPrice'];
+
+// database insert SQL code
+
+$sql = "INSERT INTO item_list (order_id, item_id, item_price, item_quantity, list_price) VALUES ('$txtOrder', '$txtItem', '$txtItemPrice', '$txtItemQuantity','$txtlistprice')";
+$rs = mysqli_query($con,$sql);
+
+$result = mysqli_query($con,"SELECT * FROM item_list");
+$bg_color = '#ff0000';
+
+echo "<form action='item_desc.html' method='post'>";
+echo "<input type='hidden' name='edit_review_id' value='$txtItem'>";
+echo "<input type='submit' value='Return'>";
+echo "</form>";
+
+
+echo "<table border='1' style='font-size: 24px'>
+<tr>
+<th>order_id</th>
+<th>item_id</th>
+<th>item_price</th>
+<th>item_quantity</th>
+<th>list_price</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['order_id'] . "</td>";
+echo "<td>" . $row['item_id'] . "</td>";
+echo "<td>" . $row['item_price'] . "</td>";
+echo "<td>" . $row['item_quantity'] . "</td>";
+echo "<td>" . $row['list_price'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
+
+
