3595cdb85ac68e64d1fff665050be4a0b8ab1c76
Catherine <catherineduong22@gmail.com>
2023-05-04 21:25:00 -0500

    commit final changes to cust_home_page and item_desc

--- admin_acc/item_edit_desc/item_desc.php
+++ admin_acc/item_edit_desc/item_desc.php
@@ -1,3 +1,4 @@
+
 <?php
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
@@ -12,18 +13,35 @@ $txtItemDesc = $_POST['txtItemDesc'];
 $txtItemPrice= $_POST['txtItemPrice'];
 
 // database insert SQL code
-$sql = "UPDATE item_3 SET item_title=$txtItemTitle, item_price=$txtItemPrice, item_desc=$txtItemDesc WHERE item_id=$txtItem";
+$sql = "UPDATE item_3 SET item_title = '$txtItemTitle', item_price = '$txtItemPrice', item_desc = '$txtItemDesc' WHERE item_id = '$txtItem'";
 
-// insert in database 
-$rs = mysqli_query($con, $sql);
+echo $txtItem;
+$rs = mysqli_query($con,$sql);
 
-if($rs)
+$result = mysqli_query($con,"SELECT * FROM item_3 WHERE item_id = '$txtItem'");
+$bg_color = '#ff0000';
+echo "<table border='1'>
+<tr>
+<th>item_id</th>
+<th>item_title</th>
+<th>item_price</th>
+<th>item_desc</th>
+<th>account_ID</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
 {
-	echo "Item's Information Updated!";
-} else {
-	echo "Error";
+echo "<tr>";
+echo "<td>" . $row['item_id'] . "</td>";
+echo "<td>" . $row['item_title'] . "</td>";
+echo "<td>" . $row['item_price'] . "</td>";
+echo "<td>" . $row['item_desc'] . "</td>";
+echo "<td>" . $row['account_ID'] . "</td>";
+echo "</tr>";
 }
+echo "</table>";
 
-$conn->close();
-
+mysqli_close($con);
 ?>
+
+
--- customer_acc/cust_home_page/cust_home_page.php
+++ customer_acc/cust_home_page/cust_home_page.php
@@ -6,14 +6,15 @@
 <body>
 <h2>Hello, this is your customers' page for items</h2>
 <h2> Select Action:
-<form action = "http://localhost/CS_310_DEMO/customer_acc/item_view_desc/item_view_desc.html">
-    <button>
-        View item's description
-    </button>
-</form>
 <form action = "http://localhost/CS_310_DEMO/customer_acc/item_list_search/item_search.html">
     <button>
-        Look up item's information
+        Look up item's information (in your order)
+    </button>
+</form>
+</form>
+<form action = "http://localhost/CS_310_DEMO/customer_acc/cust_home_page/cust_home_page.php">
+    <button>
+        Click here to get back to home
     </button>
 </form>
 </h2>
