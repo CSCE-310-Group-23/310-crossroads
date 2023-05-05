950f5e34bc8f3380848d49583c9bd1446439d716
r.j. kafka <49912871+rkafka@users.noreply.github.com>
2023-05-05 06:35:36 -0500

    Add files via upload

--- backend_crossroads/crossroads.sql
+++ backend_crossroads/crossroads.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2023 at 11:42 AM
+-- Generation Time: May 05, 2023 at 01:34 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -56,20 +56,26 @@ INSERT INTO `account` (`account_id`, `account_fname`, `account_lname`, `account_
 
 CREATE TABLE `address` (
   `address_id` int(12) NOT NULL,
+  `addr_street_num` int(7) NOT NULL,
   `addr_street_name` varchar(50) NOT NULL,
+  `addr_unit_type` varchar(20) DEFAULT NULL,
   `addr_unit_num` int(15) DEFAULT NULL,
   `addr_state` varchar(2) NOT NULL,
-  `addr_zipcode` int(5) NOT NULL,
-  `addr_street_num` int(7) NOT NULL,
-  `addr_unit_type` varchar(20) NOT NULL
+  `addr_zipcode` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `address`
 --
 
-INSERT INTO `address` (`address_id`, `addr_street_name`, `addr_unit_num`, `addr_state`, `addr_zipcode`, `addr_street_num`, `addr_unit_type`) VALUES
-(1, 'Salt Street', 3, 'TX', 75093, 333, 'APT.');
+INSERT INTO `address` (`address_id`, `addr_street_num`, `addr_street_name`, `addr_unit_type`, `addr_unit_num`, `addr_state`, `addr_zipcode`) VALUES
+(1, 333, 'Salt Street', 'APT.', 3, 'TX', 75093),
+(2, 202, 'Bright Boulevard', 'APT.', 22, 'TN', 22222),
+(3, 303, 'Sunny Streams', 'UNIT', 13, 'NY', 33013),
+(4, 1520, 'Brophy Ave', NULL, NULL, 'IL', 60646),
+(5, 907, 'Cross St', 'APT.', 817, 'TX', 77840),
+(6, 23, 'Jordan Cir', NULL, NULL, 'IL', 24086),
+(7, 922, 'Uni Dr', NULL, NULL, 'CA', 90837);
 
 -- --------------------------------------------------------
 
@@ -88,7 +94,13 @@ CREATE TABLE `address_label` (
 --
 
 INSERT INTO `address_label` (`address_id`, `account_id`, `address_title`) VALUES
-(1, 3, 'Seller Home');
+(1, 3, 'Seller Home'),
+(2, 2, 'Beef Lounge'),
+(3, 3, 'Summer Stay'),
+(4, 4, 'Family Home'),
+(5, 4, 'UCentre'),
+(6, 2, 'Baller\'s Court'),
+(7, 2, 'Dorm');
 
 -- --------------------------------------------------------
 
@@ -125,6 +137,15 @@ CREATE TABLE `item_list` (
   `list_price` int(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `item_list`
+--
+
+INSERT INTO `item_list` (`order_id`, `item_id`, `item_quantity`, `list_price`) VALUES
+(2, 1, 6, 12),
+(3, 1, 2, 4),
+(4, 2, 2, 90);
+
 -- --------------------------------------------------------
 
 --
@@ -143,7 +164,14 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`order_id`, `account_id`, `delivery_date`, `address_id`) VALUES
-(1, 3, '2023-05-10', 1);
+(1, 3, '2023-05-10', 1),
+(2, 2, '2023-05-22', 2),
+(3, 4, '2023-05-18', 4),
+(4, 4, '2023-05-09', 5),
+(5, 2, '2023-05-30', 2),
+(6, 2, '2023-06-01', 6),
+(7, 2, '2023-08-09', 7),
+(23, 2, '2023-05-30', 6);
 
 -- --------------------------------------------------------
 
