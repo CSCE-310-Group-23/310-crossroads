fea5208934a7237e767825792ed72bf453aa0d05
YvanTonthat <yvan.tonthat@tamu.edu>
2023-05-05 09:31:01 -0500

    updating progress in merging

--- customer_acc/item_list_search/item_connect.php
+++ customer_acc/item_list_search/item_connect.php
@@ -6,9 +6,15 @@ if (mysqli_connect_errno())
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 $txtOrder = $_POST['txtOrder'];
-$result = mysqli_query($con,"SELECT * FROM item_list_1 WHERE order_ID = $txtOrder");
+$result = mysqli_query($con,"SELECT * FROM item_list WHERE order_ID = $txtOrder");
 $bg_color = '#ff0000';
-echo "<table border='1'>
+
+echo "<form action='item_search.html' method='post'>";
+echo "<input type='hidden' name='edit_review_id' value='$txtOrder'>";
+echo "<input type='submit' value='Return'>";
+echo "</form>";
+
+echo "<table border='1' style='font-size: 24px'>
 <tr>
 <th>item_id</th>
 <th>item_price</th>
