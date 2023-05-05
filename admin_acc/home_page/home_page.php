7e621d2314c2ed613825d1d0db954ff527c7bdd5
Catherine <catherineduong22@gmail.com>
2023-05-05 17:43:18 -0500

    update home page

--- admin_acc/home_page/home_page.php
+++ admin_acc/home_page/home_page.php
@@ -1,11 +1,11 @@
 <?php
-$con=mysqli_connect('localhost', 'root', '','CS_310_final_project');
+$con=mysqli_connect('localhost', 'root', '','crossroads');
 // Check connection
 if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-$result = mysqli_query($con,"SELECT * FROM item_3 where account_ID ='95096179'");
+$result = mysqli_query($con,"SELECT * FROM item where account_id ='3'");
 $bg_color = '#ff0000';
 echo "<table border='1'>
 <tr>
@@ -13,7 +13,7 @@ echo "<table border='1'>
 <th>item_title</th>
 <th>item_price</th>
 <th>item_desc</th>
-<th>account_ID</th>
+<th>account_id</th>
 </tr>";
 
 while($row = mysqli_fetch_array($result))
@@ -23,7 +23,7 @@ echo "<td>" . $row['item_id'] . "</td>";
 echo "<td>" . $row['item_title'] . "</td>";
 echo "<td>" . $row['item_price'] . "</td>";
 echo "<td>" . $row['item_desc'] . "</td>";
-echo "<td>" . $row['account_ID'] . "</td>";
+echo "<td>" . $row['account_id'] . "</td>";
 echo "</tr>";
 }
 echo "</table>";
--- admin_acc/home_page/home_page_main.php
+++ admin_acc/home_page/home_page_main.php
@@ -7,22 +7,22 @@
 <h2>Hello, this is your admin page for items</h2>
 <?php include 'home_page.php'; ?>
 <h2> Select Action:
-<form action = "http://localhost/CS_310_DEMO/admin_acc/item_delete/item_delete.html">
+<form action = "http://localhost/CSCE310/310-crossroads/admin_acc/item_delete/item_delete.html">
     <button>
         Delete An Item
     </button>
 </form>
-<form action = "http://localhost/CS_310_DEMO/admin_acc/item_edit_desc/item_desc.html">
+<form action = "http://localhost/CSCE310/310-crossroads/admin_acc/item_edit_desc/item_desc.html">
     <button>
         Edit Item Description
     </button>
 </form>
-<form action = "http://localhost/CS_310_DEMO/admin_acc/item_list_insertion/item_list.html">
+<form action = "http://localhost/CSCE310/310-crossroads/admin_acc/item_list_insertion/item_list.html">
     <button>
         Add New Item
     </button>
 </form>
-<form action = "http://localhost/CS_310_DEMO/customer_acc/cust_home_page/cust_home_page.php">
+<form action = "http://localhost/CSCE310/310-crossroads/customer_acc/cust_home_page/cust_home_page.php">
     <button>
         Switch To Customer's View
     </button>
